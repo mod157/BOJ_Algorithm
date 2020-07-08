7c29e9461aecb7f84f389068b96281293255de52
Nammu <mod157@naver.com>
2020-07-08 17:36:25 +0900

    No.11117 Java, Kotlin Upload

--- No.11117/Answer.java
+++ No.11117/Answer.java
@@ -1,8 +1,52 @@
 import java.util.Scanner;
 
 public class Answer{
-
     public static void main(String[] args){
-        System.out.println("Hello");
+        Scanner scan = new Scanner(System.in);
+        int i = 0, j = 0, k = 0;
+        int[] TNum = new int[26];
+        int[] TNum_Checking = new int[26];
+        String str;
+
+        int cntT = scan.nextInt();
+        int cntW = 0;
+        boolean isAnswer = false;
+
+        for(i = 0 ; i < cntT; i++)
+        {
+            str = scan.next();
+            for(j = 0; j < TNum.length; j++){
+                TNum[j] = 0;
+            }
+
+            for(j = 0 ; j < str.length(); j++)
+            {
+                TNum[str.charAt(j) - 'A']++;
+            }
+
+            cntW = scan.nextInt();
+            String[] spells = new String[cntW];
+            
+            for(j = 0 ; j < cntW; j++)
+            {
+                spells[j] = scan.next();
+            }
+
+            for(j = 0 ; j < cntW; j++){
+                TNum_Checking = TNum.clone();
+                for(k = 0; k < spells[j].length(); k++){
+                    if(--TNum_Checking[spells[j].charAt(k) - 'A'] < 0)
+                    {
+                        System.out.println("NO");
+                        isAnswer = true;
+                        break;
+                    }
+                }
+                if(!isAnswer){
+                    System.out.println("YES");
+                }
+                isAnswer = false;
+            }
+        }
     }
 }
--- No.11117/Answer.kt
+++ No.11117/Answer.kt
@@ -1,13 +1,38 @@
 import java.util.*
 
-fun greeting() {
-   println("Hello World!")
-}
-
-fun main(args: Array<String>) {
-   greeting()
-}
-/* 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
-   println("AAA");
-}*/
+   val cntT = nextInt()
+   var TNum = Array(26, { 0 })
+   var isAnswer = false;
+   
+   for(i in 1..cntT ){
+      val str = next()
+      TNum = Array(26, { 0 })
+
+      for(c in str){
+         TNum[c.toChar() - 'A']++
+      }
+
+      val cntW = nextInt()
+      var spells:Array<String> = Array(cntW) { "" }
+
+      for(n in 0 until cntW){
+         spells[n] = next() 
+      }
+
+      for(n in 0 until cntW){
+         var TNum_Checking = TNum.clone()
+         for(j in 0 until spells[n].length){
+            if(--TNum_Checking[spells[n][j] - 'A'] < 0){
+               println("NO")
+               isAnswer = true
+               break;
+            }
+         }
+         if(!isAnswer){
+            println("YES")
+         }
+         isAnswer = false
+      }
+   }
+}
