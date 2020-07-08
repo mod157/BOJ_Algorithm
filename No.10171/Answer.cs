6583a118ef3c40fedd085a608ee89f15626e7d23
Nammu <mod157@naver.com>
2020-07-08 17:42:50 +0900

    No.10171 Upload

---
+++ No.10171/Answer.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Answer{
+    static void Main(string[] args)
+    {
+        Console.Write("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+    }
+}
---
+++ No.10171/Answer.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+   println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|")
+}
